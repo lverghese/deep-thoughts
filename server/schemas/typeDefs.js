816db7286c2aa1cdce1b8f840ba679c02cf1fc33
lverghese <loganverghese@gmail.com>
2021-11-01 23:36:15 -0400

    different mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,28 +13,42 @@ const typeDefs = gql`
         friends: [User]
     }
 
-  type Thought {
-    _id: ID
-    thoughtText: String
-    createdAt: String
-    username: String
-    reactionCount: Int
-    reactions: [Reaction]
-  }
+    type Thought {
+        _id: ID
+        thoughtText: String
+        createdAt: String
+        username: String
+        reactionCount: Int
+        reactions: [Reaction]
+    }
 
-  type Reaction {
-    _id: ID
-    reactionBody: String
-    createdAt: String
-    username: String
-  }
+    type Reaction {
+        _id: ID
+        reactionBody: String
+        createdAt: String
+        username: String
+    }
 
-  type Query {
-    users: [User]
-    user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(_id: ID!): Thought
-  }
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+        thoughts(username: String): [Thought]
+        thought(_id: ID!): Thought
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addThought(thoughtText: String!): Thought
+        addReaction(thoughtId: ID!, reactionBody: String!): Thought
+        addFriend(friendId: ID!): User
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
 `;
 
 // export the typeDefs
